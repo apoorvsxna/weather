fb5eec0b9f08fb9dd44f5842b29fb26c54e7180d
Apoorv Saxena <112375644+apoorvsxna@users.noreply.github.com>
2024-01-13 01:40:09 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,19 +8,22 @@ const searchTemperature = () => {
         .then(data => displayTemperature(data));
 }
 
-const setInnerText = (id, text) =>{
+const setInnerText = (id, text) => {
     document.getElementById(id).innerText = text;
 }
-
 
 const displayTemperature = temperature => {
     console.log(temperature);
     setInnerText('city', temperature.name);
-    setInnerText('temp', temperature.main.temp);
-    setInnerText('weather', temperature.weather[0].main);
+    setInnerText('temp', temperature.main.temp + '°C');
+    setInnerText('weather', temperature.weather[0].description);
 
-    // weather icon settings 
-    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    // Display additional weather information
+    setInnerText('humidity', 'Humidity: ' + temperature.main.humidity + '%');
+    setInnerText('wind', 'Wind Speed: ' + temperature.wind.speed + ' m/s');
+
+    // weather icon settings
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
     const imgIcon = document.getElementById('image-icon');
     imgIcon.setAttribute('src', url);
 }
